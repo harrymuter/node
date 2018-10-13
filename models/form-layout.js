b0b9d94145a69b18f4a35e25ea34ef439ecdb6ff
Harry Muter <harry.muter95@gmail.com>
2018-10-13 02:09:19 +0100

    Database changes

--- models/form-layout.js
+++ models/form-layout.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/nodeauth', { useNewUrlParser: true });
+mongoose.connect('mongodb://178.62.8.45:27017/nodeauth', { useNewUrlParser: true });
 
     const FormLayoutSchema = mongoose.Schema({
         form_layout_no: {
